7862aac32950b9ff1a142c02a69049c0bab89a78
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-08-04 12:07:40 +0530

    Update Page.js

--- Routing/Page.js
+++ Routing/Page.js
@@ -1,1 +1,20 @@
+// Here the simple example of Routing in React.
+import React from "react";
+
+
+const page = () =>{
+  return (
+    <>
+
+    <div className="px-3 mt-3 flex flex-col">
+     
+      <h1>This is home page</h1>
+      <h2>build with React</h2>
+   
+    </div>
+
+    </>
+  )
+}
+export default page;
 
