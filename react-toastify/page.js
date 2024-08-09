7460c7009f276093cdf9999b4fbce7db9f56deae
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-08-09 17:25:52 +0530

    Update page.js

--- react-toastify/page.js
+++ react-toastify/page.js
@@ -1,1 +1,19 @@
+"use client"
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
+
+const page = () => {
+  const notify = () =>{
+    toast("login successful")
+  }
+  return (
+    <div>
+      <button onClick = {notify}className='px-5 py-3 mx-5 my-5 rounded text-black bg-slate-300 border-gray-400'>Login</button>
+      <ToastContainer />
+    </div>
+  )
+}
+
+export default page;
